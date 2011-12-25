0d23d6b9a1f61125289e5ab4c8f3c257b7107184
Markus Bachmann <info@bachi.biz>
2011-12-25 01:51:38 +0100

    Add response test

---
+++ tests/CouchDB/Tests/Http/Response/ResponseTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace CouchDB\Tests\Http\Response;
+
+use CouchDB\Http\Response\Response;
+use CouchDB\Tests\TestCase;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class ResponseTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->response = new Response(200, '{"status:"404"}', array('Content-Type' => 'application/json'));
+    }
+
+    public function testGetContent()
+    {
+        $this->assertEquals('{"status:"404"}', $this->response->getContent());
+    }
+
+    public function testGetStatusCode()
+    {
+        $this->assertEquals(200, $this->response->getStatusCode());
+    }
+
+    public function testGetHeader()
+    {
+        $this->assertEquals('application/json', $this->response->getHeader('Content-Type'));
+        $this->assertNull($this->response->getHeader('not exist'));
+    }
+
+    public function testGetHeaders()
+    {
+        $this->assertEquals(array('Content-Type' => 'application/json'), $this->response->getHeaders());
+    }
+
+    public function testIsSuccessful()
+    {
+        $this->assertTrue($this->response->isSuccessful());
+        $response = new Response(500, '', array());
+        $this->assertFalse($response->isSuccessful());
+    }
+}
