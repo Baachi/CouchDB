d366f7986ee0d6fe4cc213acc1a8bfcfe61f47c4
Markus Bachmann <info@bachi.biz>
2011-12-25 02:14:01 +0100

    Add Exceptions

---
+++ src/CouchDB/Exception/JsonEncodeException.php
@@ -0,0 +1,24 @@
+<?php
+namespace CouchDB\Exception;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class JsonDecodeException extends \Exception
+{
+    protected static $errors = array(
+        JSON_ERROR_NONE           => 'unknown error',
+        JSON_ERROR_DEPTH          => 'The maximum stack depth has been exceeded',
+        JSON_ERROR_SYNTAX         => 'Syntax error',
+        JSON_ERROR_CTRL_CHAR      => 'Control character error',
+        JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+        JSON_ERROR_UTF8           => 'Malformed UTF-8 characters'
+    );
+
+    public function __construct($data, $code, $previous)
+    {
+        $message = sprintf('Json encode error [%s]: %s', static::$errors[json_last_error()], var_export($data, true));
+        parent::__construct($message, $code, $previous);
+    }
+
+}
