992a67d5c5d129c523abf8c0247982e52bc9e567
Markus Bachmann <info@bachi.biz>
2011-12-25 22:55:44 +0100

    Add unit test for event args

---
+++ tests/CouchDB/Tests/Events/EventArgsTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace CouchDB\Tests\Events;
+
+use CouchDB\Tests\TestCase;
+use CouchDB\Events\EventArgs;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+class EventArgsTest extends TestCase
+{
+    public function testGetInvoker()
+    {
+        $invoker = new Dummy();
+        $args = new EventArgs($invoker);
+        $this->assertEquals($invoker, $args->getInvoker());
+        $this->assertEquals('bar', $args->getInvoker()->foo);
+        $invoker->foo = 'foobar';
+        $this->assertEquals('foobar', $args->getInvoker()->foo);
+    }
+
+    public function testGetData()
+    {
+        $data = 'foo';
+        $args = new EventArgs(new Dummy(), $data);
+        $this->assertEquals('foo', $args->getData());
+    }
+}
+
+class Dummy
+{
+    public $foo = 'bar';
+}
