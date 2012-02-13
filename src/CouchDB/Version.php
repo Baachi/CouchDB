f6486eefd0fb4a9e0fea35fefc4992ec34368567
Markus Bachmann <info@bachi.biz>
2012-02-13 20:55:55 +0100

    Add Version class

---
+++ src/CouchDB/Version.php
@@ -0,0 +1,21 @@
+<?php
+namespace CouchDB;
+
+/**
+ * @author Markus Bachmann <markus.bachmann@bachi.biz>
+ */
+final class Version
+{
+    const VERSION = '0.8';
+
+    /**
+     * Compare a given version with the current version
+     *
+     * @param string $version
+     * @return integer
+     */
+    public static function compare($version)
+    {
+        return version_compare(self::VERSION, $version);
+    }
+}
