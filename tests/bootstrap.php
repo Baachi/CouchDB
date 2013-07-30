2fff00ad3d6d4c70c2baf68f7f09eda6f4223dfa
Markus Bachmann <info@bachi.biz>
2013-07-30 19:20:40 +0200

    Fix testsuite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
-if (file_exists(__DIR__ . '/../autoload.php')) {
-    require_once __DIR__ . '/../autoload.php';
-} else {
-    require_once __DIR__ . '/../autoload.php.dist';
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    echo 'You must run "composer.phar install" to run the testsuite!';
+    die();
 }
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader->add('CouchDB\\Tests', __DIR__);
