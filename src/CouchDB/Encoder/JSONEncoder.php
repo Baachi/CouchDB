2b90fa6904d6f41ebdfcaff18e4d01ed65765fe0
Baachi <info@bachi.biz>
2015-07-01 18:36:00 +0200

    Improve error handling in JSONEncoder

--- src/CouchDB/Encoder/JSONEncoder.php
+++ src/CouchDB/Encoder/JSONEncoder.php
@@ -13,24 +13,34 @@ final class JSONEncoder
 
     public static function encode($value)
     {
-        $json = @json_encode($value);
+        set_error_handler(function() use ($value) {
+            throw new JsonEncodeException($value);
+        });
+
+        $json = json_encode($value);
+
         if ("null" === $json && $value !== null) {
             throw new JsonEncodeException($value);
         }
-        if (false === $json) {
-            throw new JsonEncodeException($value);
-        }
+
+        restore_error_handler();
 
         return $json;
     }
 
     public static function decode($json)
     {
+        set_error_handler(function() use ($json) {
+            throw new JsonDecodeException($json);
+        });
+
         $value = json_decode($json, true);
 
         if (false === $value) {
             throw new JsonDecodeException($json);
         }
+
+        restore_error_handler();
 
         return $value;
     }
