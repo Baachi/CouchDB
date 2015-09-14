20e6116a66195b0589c89ca688118e819d193a5a
Baachi <info@bachi.biz>
2015-09-14 22:29:16 +0200

    Fix JsonException

--- src/CouchDB/Exception/JsonException.php
+++ src/CouchDB/Exception/JsonException.php
@@ -7,10 +7,12 @@ namespace CouchDB\Exception;
  */
 class JsonException extends \Exception
 {
-    public function __construct($data, $code = 0, $previous = null)
-    {
-        $message = sprintf('Json encode error [%s]: %s', self::$errors[json_last_error()], var_export($data, true));
-
-        parent::__construct($message, $code, $previous);
-    }
+    protected static $errors = [
+        JSON_ERROR_NONE           => 'unknown error',
+        JSON_ERROR_DEPTH          => 'The maximum stack depth has been exceeded',
+        JSON_ERROR_SYNTAX         => 'Syntax error',
+        JSON_ERROR_CTRL_CHAR      => 'Control character error',
+        JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+        JSON_ERROR_UTF8           => 'Malformed UTF-8 characters',
+    ];
 }
